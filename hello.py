facd6e529750e5c8593b70d327c6a331398388dc
lizbakerdrewu <77995259+lizbakerdrewu@users.noreply.github.com>
2021-01-30 14:28:23 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -33,6 +33,6 @@ def mix(a, b):
 
 def main():
     print("squared sum: ", squared_sum([-3, 4]))
-    print("mix:         ", mix("1234", "abcd") == "1a2b3c4d")
+    print("mix: ", mix("1234", "abcd"))
 
 main()
